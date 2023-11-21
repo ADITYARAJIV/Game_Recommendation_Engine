a294940c84d1a9440fade9ee9c24531bd8556da7
Aditya Rajiv <adimantheboss123@gmail.com>
2023-11-21 11:25:00 +0530

    updated app.py changed game_dict to games_dict

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 from game_recommender_engine import recommendation_system
 
-game_dict = pickle.load(open('games_dict.pkl', 'rb'))
+games_dict = pickle.load(open('games_dict.pkl', 'rb'))
 game = pd.DataFrame(games_dict)
 ft_vector = pickle.load(open('ft_vector.pkl', 'rb'))
 ft_model = pickle.load(open('ft_model.pkl', 'rb'))
